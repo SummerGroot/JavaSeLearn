449695229444a8a98678841e21a065dd93b50062
Summer <528150506@qq.com>
2022-11-22 20:40:19 +0800

    learn_01

---
+++ JavaBasic/src/com/basic/www/conpter01/CharDetail.java
@@ -0,0 +1,17 @@
+package com.basic.www.conpter01;
+
+/**
+ * @version: java version 1.8
+ * @Author: Summer Johnny
+ * @description:
+ * @date: 2022-11-22 20:23
+ */
+public class CharDetail {
+    public static void main(String[] args) {
+        char c1 = 'a';
+        char c2 = '\t';
+        char c3 = '夏';
+        char c4 = 97;
+    }
+}
+
