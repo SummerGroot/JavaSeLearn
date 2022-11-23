08edecef60c929c552ce78a038e8904f8e89f354
summer <528150506@qq.com>
2022-11-23 15:38:52 +0800

    learn

---
+++ JavaBasic/src/com/basic/www/conpter01/AutoConvertDetail.java
@@ -0,0 +1,13 @@
+package com.basic.www.conpter01;
+
+/**
+ * @version: java version 1.8
+ * @Author: Summer
+ * @description:
+ * @date: 2022-11-23 15:37
+ */
+public class AutoConvertDetail {
+    public static void main(String[] args) {
+        //
+    }
+}
---
+++ JavaBasic/src/com/basic/www/conpter01/Boolean.java
@@ -0,0 +1,19 @@
+package com.basic.www.conpter01;
+
+/**
+ * @version: java version 1.8
+ * @Author: Summer
+ * @description:
+ * @date: 2022-11-23 15:14
+ */
+public class Boolean {
+    public static void main(String[] args) {
+        //定义一个布尔变量
+        boolean pass = false;
+        if(pass == true){
+            System.out.println("考试通过");
+        }else {
+            System.out.println("考试未通过");
+        }
+    }
+}
