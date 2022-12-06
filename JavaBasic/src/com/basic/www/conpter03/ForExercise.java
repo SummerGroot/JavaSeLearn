854fe95f9d86d13849886542dfb7959aa60a8d5d
summer <528150506@qq.com>
2022-12-06 22:18:30 +0800

    learn

---
+++ JavaBasic/src/com/basic/www/conpter03/ForExercise.java
@@ -0,0 +1,20 @@
+package com.basic.www.conpter03;
+
+/**
+ * @version: java version 1.8
+ * @Author: Summer
+ * @description:
+ * @date: 2022-12-06 22:09
+ */
+public class ForExercise {
+    public static void main(String[] args) {
+        //1、打印1~100之间所有是9的倍数的整数，统计个数 及总和
+        int count = 0;
+        int number = 0;
+        for (int i = 1; i <= 100; i++) {
+            //System.out.println("i="+i);
+        }
+        //System.out.println("9的倍数有：" + number + "总计为：" + count);
+        //2、完成下面的表达式输出
+    }
+}
