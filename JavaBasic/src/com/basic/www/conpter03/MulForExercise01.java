317720eb9b893bed3e8e0d464392838a8e32920c
summer <528150506@qq.com>
2022-12-07 15:41:31 +0800

    java

--- JavaBasic/src/com/basic/www/conpter03/MulForExercise01.java
+++ JavaBasic/src/com/basic/www/conpter03/MulForExercise01.java
@@ -11,18 +11,26 @@ import java.util.zip.CheckedOutputStream;
  */
 public class MulForExercise01 {
     public static void main(String[] args) {
-        //统计3个班成绩情况，每个班有5名同学，求出各个班的平均分和所有班级的平均分
+        //统计3个班成绩情况，每个班有5名同学，
+        // 求出各个班的平均分和所有班级的平均分
         //成绩从键盘输入
-        int count = 0;
+        /*
+         * 1、先计算一个班，5个学生的成绩，使用for
+         * 1.1创建一个Scanner对象，接受用户输入
+         * 1.2得到该班级的平均分，定义double count把5个学生成绩累计*/
+        double count = 0;
         Scanner input = new Scanner(System.in);
-        for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.println("请输入成绩：");
+        for (int i = 1; i <= 1; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.println("请输入：" + j + "个对学生成绩：");
                 double score = input.nextDouble();
+                System.out.println("成绩为：" + score);
                 count += score;
             }
-            System.out.println(count);
+            //count是5个学生的总成绩
+            
             System.out.println("班级平均分为：" + count / 5);
         }
+        //System.out.println("年级平均分：" + count / 5);
     }
 }
