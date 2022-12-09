05ce42705cc32642dcb13b542fcd466849bf6c4b
summer <528150506@qq.com>
2022-12-09 10:34:42 +0800

    javas

---
+++ JavaBasic/src/com/basic/www/conpter03/Stars.java
@@ -0,0 +1,34 @@
+package com.basic.www.conpter03;
+
+/**
+ * @version: java version 1.8
+ * @Author: Summer Johnny
+ * @description:
+ * @date: 2022-12-09 10:27
+ */
+public class Stars {
+    public static void main(String[] args) {
+        /*
+         *       *
+         *      * *
+         *     *   *
+         *    *******
+         *思路分析
+         *化繁为简
+         * 1、先打印矩形
+         * *****
+         * *****
+         * *****
+         * *****
+         * 2、打印半个金字塔
+         * *  //第一层
+         * **
+         * ***
+         * ****
+         * *****
+         * */
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("*****");
+        }
+    }
+}
